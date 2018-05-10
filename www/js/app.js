0a7a8e53b3beb1a833ceee26f4e6ec99a2d021ea
foolingdutchman <365300312@qq.com>
2018-05-10 14:06:29 +0800

    login

--- www/js/app.js
+++ www/js/app.js
@@ -21,6 +21,36 @@ var app  = new Framework7({
     helloWorld: function () {
       app.dialog.alert('Hello World!');
     },
+
+    request: function(url,data,callback){
+        var xmlhttp;
+        if (window.XMLHttpRequest)
+          {// code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp=new XMLHttpRequest();
+          }
+         else
+          {// code for IE6, IE5
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+           } 
+           xmlhttp.onreadystatechange=function()
+       {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+   var response= xmlhttp.responseText;
+   callback.handle(response);
+    }
+  }
+     xmlhttp.open("POST","http://gstdev.tagq.org/api/v1/"+url,true);
+     xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+     xmlhttp.send();
+    },
+    sgin: function(phone,password){
+      var data="phone="+phone+"&password="+password+"&sign_type=sign_in";
+      this.request("users/sign",data,function(response){
+         app.dialog.alert('response: ' + response);
+      });
+
+    }
   },
   // App routes
   routes: routes,
@@ -33,12 +63,13 @@ var mainView = app.views.create('.view-main', {
 
 // Login Screen Demo
 $$('#my-login-screen .login-button').on('click', function () {
-  var username = $$('#my-login-screen [name="username"]').val();
+  var phone = $$('#my-login-screen [name="phone"]').val();
   var password = $$('#my-login-screen [name="password"]').val();
-
   // Close login screen
-  app.loginScreen.close('#my-login-screen');
-
+  // app.loginScreen.close('#my-login-screen');
+  
+  
   // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+  app.dialog.alert('phone: ' + phone + '<br>Password: ' + password);
+  app.methods.sgin(phone,password);
 });
